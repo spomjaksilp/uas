Moves trajectory calculation out of main loop, fixes #4
Trajectories can now be calculated afterwards

src/uas/move.py
@@ -3,101 +3,23 @@
 """
 
 import numpy as np
-from numba import types, typed, typeof, njit
-from numba.experimental import jitclass
 from uas import Lattice
-from uas.helper import type_coordinate, type_path_matrix
-
-spec_move = [
-    ("origin", types.int16[:]),
-    ("target", types.int16[:]),
-    ("path", types.ListType(type_coordinate)),
-]
-
-
-@njit((types.ListType(type_path_matrix))(types.float32[:], types.float32[:]), fastmath=True)
-def l_path(origin, target):
-    path = typed.List.empty_list(type_path_matrix)
-    # path.append(origin)
-    difference = origin - target
-    pointer = np.copy(origin)
-    # moves
-    pointer[0] = pointer[0] - difference[0]
-    path.append(np.stack((origin, pointer)))
-    path.append(np.stack((pointer, target)))
-    return path
 
 
 class Move:
     """
     Defines a single move from a origin coordinate to a target coordinate
     """
-
     def __init__(self, origin: np.ndarray, target: np.ndarray):
         self.origin = origin
         self.target = target
-        self.path = []
-        # self.path = typed.List.empty_list(type_coordinate)  # numba
-        self.calculate_moves()
-
-    def calculate_moves(self):
-        """
-        Calculates the internal trajectory
-        :return:
-        """
-        raise NotImplementedError
 
 
-# @jitclass(spec_move)
 class Discard(Move):
     """
     Discard an atom in a site
     """
-
-    def calculate_moves(self):
-        self.path.append(np.stack((self.origin, self.origin)))
-
-
-class Type1(Move):
-    """
-    Type 1 move according to Léséleuc (2018).
-    Describes a move in between sites
-    """
-    def calculate_moves(self):
-        self.path = self._calculate_moves(self.origin.astype(np.float32), self.target.astype(np.float32))
-
-    @staticmethod
-    @njit((types.ListType(type_path_matrix))(types.float32[:], types.float32[:]), fastmath=True)
-    def _calculate_moves(origin, target):
-        path = typed.List.empty_list(type_path_matrix)
-        # path.append(origin)
-        difference = target - origin
-        diagonal_step = difference / np.abs(difference) / 2
-        # handle edge case: straight line, i.e. one component of difference == 0
-        for i in range(2):
-            if difference[i] == 0:
-                diagonal_step[i] = - 0.5
-        # moves
-        # diagonals move (step outside grid)
-        post_origin = np.abs(origin + diagonal_step)
-        pre_target = np.abs(target - diagonal_step)
-        path.append(np.stack((origin, post_origin)))
-        # L shaped path between the two intermediate points
-        l_x, l_y = l_path(post_origin, pre_target)
-        path.append(l_x)
-        path.append(l_y)
-        path.append(np.stack((pre_target, target)))
-        return path
-
-
-# @jitclass(spec_move)
-class Type2(Move):
-    """
-    Type 2 move according to Léséleuc (2018).
-    Describes a move on the connecting edges of sites
-    """
-    def calculate_moves(self):
-        self.path = l_path(self.origin.astype(np.float32), self.target.astype(np.float32))
+    pass
 
 
 class Plan:

src/uas/strategy/french.py
@@ -5,7 +5,7 @@ From Paris
 import numpy as np
 from scipy.spatial import distance
 from numba import njit, types, typeof, typed
-from uas import Lattice, Plan, Type1, Type2
+from uas import Lattice, Plan
 from uas.helper import type_coordinate, type_coordinate_matrix, type_site_matrix
 from . import StrategyTemplate
 
@@ -19,11 +19,11 @@ class French(StrategyTemplate):
     * calculate moves
     """
 
-    def __init__(self, start: Lattice, target: Lattice, planner: Plan):
+    def __init__(self, start: Lattice, target: Lattice):
         self.start = start
         self.current_state = start
         self.target = target
-        self.plan = planner(Type1)
+        self.plan = Plan()
         self.report = {}
         # first assert that a possible solution exists
         assert np.sum(start.value) >= np.sum(target.value), f"Unsolvable {np.sum(start.value)} sites cannot be sorted" \